d8c52bfc5f4a43b5d464b8c1dd9029943936afbe
Ismaeel Mughal <ismaeel.mughal.1912@gmail.com>
2023-01-07 19:34:48 +0500

    Common Layer Added with entity Framework

---
+++ Common/ApplicationDB.cs
@@ -0,0 +1,25 @@
+﻿using Common.Models;
+using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Common
+{
+    public class ApplicationDB : DbContext
+    {
+
+        public ApplicationDB() : base("name=connectionString")
+        {
+            Database.SetInitializer<ApplicationDB>(
+                new DropCreateDatabaseIfModelChanges<ApplicationDB>());
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<TodoTasks> Tasks { get; set; }
+    }
+}
